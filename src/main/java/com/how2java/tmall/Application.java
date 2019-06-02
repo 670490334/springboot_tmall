c1b01d4221c3fe49b66eea489349329c68d4ca48
670490334 <37465763+670490334@users.noreply.github.com>
2019-06-02 12:10:04 +0800

    Update Application.java

--- src/main/java/com/how2java/tmall/Application.java
+++ src/main/java/com/how2java/tmall/Application.java
@@ -2,7 +2,7 @@ package com.how2java.tmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//springboot 商城后台管理
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
